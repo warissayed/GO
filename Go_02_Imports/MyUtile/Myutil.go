aea08459984228900a998b9b7efc10db03cc7281
warissayed <warissayed72@gmail.com>
2025-09-18 20:00:20 +0530

    day 1 18/09/2025 Learning go file management system

---
+++ Go_02_Imports/MyUtile/Myutil.go
@@ -0,0 +1,9 @@
+package MyUtil
+
+import (
+	"fmt"
+)
+
+func MyUtil(message string) {
+	fmt.Println(message)
+}
---
+++ Go_02_Imports/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	MyUtil "imports/MyUtile"
+)
+
+func main() {
+	fmt.Printf("Learning Imports")
+	MyUtil.MyUtil("Packages are tightly Coupled with the folders ")
+}
