d7b8fd574f5f873d45966b433782e217eecbbf9b
warissayed <warissayed72@gmail.com>
2025-10-17 17:34:30 +0530

    Learn about types and Array and slices and map

---
+++ 04_Types/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Strings,Bool, Integer:- uint8 uint64 int8 int64 uintptr, Floating:- float32 float64 , Complex:- complex64 complex128")
+	fmt.Println("Complex type in Golang : Array,Slices,Maps, Structs, Pointers & there are function and channels ")
+}
